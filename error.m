e2fbd71c762536b6af771dff4c0411350dd0d43c
Pradyuman Vig <pradyumanvig@hotmail.com>
2015-05-01 22:02:30 -0400

    finished condensing error GUI

--- error.m
+++ error.m
@@ -1,4 +1,4 @@
-function varargout = errorGUI_sec13_team18(varargin)
+function varargout = error(varargin)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 %  ENGR 13200 Spring 2015
 %  Programmer(s) and Purdue Email Address(es): 
@@ -28,20 +28,20 @@ function varargout = errorGUI_sec13_team18(varargin)
 %  text that is the input argument sent from an individual GUI.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%ERRORGUI_SEC13_TEAM18 M-file for errorGUI_sec13_team18.fig
-%      ERRORGUI_SEC13_TEAM18, by itself, creates a new ERRORGUI_SEC13_TEAM18 or raises the existing
+%ERROR M-file for error.fig
+%      ERROR, by itself, creates a new ERROR or raises the existing
 %      singleton*.
 %
-%      H = ERRORGUI_SEC13_TEAM18 returns the handle to a new ERRORGUI_SEC13_TEAM18 or the handle to
+%      H = ERROR returns the handle to a new ERROR or the handle to
 %      the existing singleton*.
 %
-%      ERRORGUI_SEC13_TEAM18('Property','Value',...) creates a new ERRORGUI_SEC13_TEAM18 using the
+%      ERROR('Property','Value',...) creates a new ERROR using the
 %      given property value pairs. Unrecognized properties are passed via
-%      varargin to errorGUI_sec13_team18_OpeningFcn.  This calling syntax produces a
+%      varargin to error_OpeningFcn.  This calling syntax produces a
 %      warning when there is an existing singleton*.
 %
-%      ERRORGUI_SEC13_TEAM18('CALLBACK') and ERRORGUI_SEC13_TEAM18('CALLBACK',hObject,...) call the
-%      local function named CALLBACK in ERRORGUI_SEC13_TEAM18.M with the given input
+%      ERROR('CALLBACK') and ERROR('CALLBACK',hObject,...) call the
+%      local function named CALLBACK in ERROR.M with the given input
 %      arguments.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
@@ -49,7 +49,7 @@ function varargout = errorGUI_sec13_team18(varargin)
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help errorGUI_sec13_team18
+% Edit the above text to modify the response to help error
 
 % Last Modified by GUIDE v2.5 07-Apr-2015 04:33:13
 
@@ -57,8 +57,8 @@ function varargout = errorGUI_sec13_team18(varargin)
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @errorGUI_sec13_team18_OpeningFcn, ...
-                   'gui_OutputFcn',  @errorGUI_sec13_team18_OutputFcn, ...
+                   'gui_OpeningFcn', @error_OpeningFcn, ...
+                   'gui_OutputFcn',  @error_OutputFcn, ...
                    'gui_LayoutFcn',  [], ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -75,8 +75,8 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before errorGUI_sec13_team18 is made visible.
-function errorGUI_sec13_team18_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before error is made visible.
+function error_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -84,7 +84,7 @@ function errorGUI_sec13_team18_OpeningFcn(hObject, eventdata, handles, varargin)
 % varargin   unrecognized PropertyName/PropertyValue pairs from the
 %            command line (see VARARGIN)
 
-% Choose default command line output for errorGUI_sec13_team18
+% Choose default command line output for error
 handles.output = hObject;
 
 % Update handles structure
@@ -96,12 +96,12 @@ if ~isempty(varargin)
     set(handles.errorMSG_st, 'String', message);
 end
 
-% UIWAIT makes errorGUI_sec13_team18 wait for user response (see UIRESUME)
+% UIWAIT makes error wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = errorGUI_sec13_team18_OutputFcn(hObject, eventdata, handles)
+function varargout = error_OutputFcn(hObject, eventdata, handles)
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -116,4 +116,4 @@ function close_pb_Callback(hObject, eventdata, handles)
 % hObject    handle to close_pb (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-close errorGUI_sec13_team18 %Close error GUI
+close error %Close error GUI
